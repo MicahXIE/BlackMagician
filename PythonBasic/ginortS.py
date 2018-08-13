5d94fb512da886ad376bb1f39515c6a01f190a5b
Micah007PKG <bin.xie.sg@gmail.com>
2018-08-13 13:14:13 +0800

    update

---
+++ PythonBasic/ginortS.py
@@ -0,0 +1,36 @@
+'''
+You are given a string S. 
+All sorted lowercase letters are ahead of uppercase letters.
+All sorted uppercase letters are ahead of digits.
+All sorted odd digits are ahead of sorted even digits.
+
+Sample Input
+
+Sorting1234
+Sample Output
+
+ginortS1324
+
+'''
+
+s = input().strip()
+
+s_lower = ''
+s_upper = ''
+s_odd = ''
+s_even = ''
+
+for c in s:
+    if c.islower():
+        s_lower += c
+    if c.isupper():
+        s_upper += c
+    if c.isdigit():
+        if int(c)%2 == 0:
+            s_even += c
+        else:
+            s_odd += c
+
+res = ''.join(sorted(s_lower)) + ''.join(sorted(s_upper)) + ''.join(sorted(s_odd)) + ''.join(sorted(s_even))
+
+print(res)
