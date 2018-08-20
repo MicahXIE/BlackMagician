3a32c81c3a460c6e77d422e985c9b02d01890590
Micah007PKG <bin.xie.sg@gmail.com>
2018-08-21 02:54:21 +0800

    update

---
+++ PythonBasic/XML2_Find_the_Maximum_Depth.py
@@ -0,0 +1,43 @@
+'''
+Sample Input
+
+6
+<feed xml:lang='en'>
+    <title>HackerRank</title>
+    <subtitle lang='en'>Programming challenges</subtitle>
+    <link rel='alternate' type='text/html' href='http://hackerrank.com/'/>
+    <updated>2013-12-25T12:00:00</updated>
+</feed>
+Sample Output
+
+1
+Explanation
+
+Here, the root is a feed tag, which has depth of 0. 
+The tags title, subtitle, link and updated all have a depth of 1. 
+
+Thus, the maximum depth is 1.
+
+'''
+
+
+import xml.etree.ElementTree as etree
+
+maxdepth = 0
+def depth(elem, level):
+    global maxdepth
+    # your code goes here
+    for child in elem:
+        depth(child, level+1)
+    
+    if maxdepth < (level+1):
+        maxdepth = level+1
+
+if __name__ == '__main__':
+    n = int(input())
+    xml = ""
+    for i in range(n):
+        xml =  xml + input() + "\n"
+    tree = etree.ElementTree(etree.fromstring(xml))
+    depth(tree.getroot(), -1)
+    print(maxdepth)
